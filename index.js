d4f5359ee4f723343c50ea75dd115e91acfb0b9f
wook3210 <wook3210@gmail.com>
2020-12-22 22:47:50 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -3,17 +3,37 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const config = require('./config/key')
+
+const bodyParser = require('body-parser')
+const {User} = require('./models/User')
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex: true , useFindAndModify:false
 })  .then(()=>console.log('MongoDB Connected...'))
     .catch(err=>console.log(err))
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => {  res.send('Hello_World!')})
+
+
+app.post('/register', (req, res)=>{
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo)=>{
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
